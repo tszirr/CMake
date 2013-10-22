91af9ad4ca8dfbe9fb34c3eb5393e7f360e7d49c
Stephen Kelly <steveire@gmail.com>
2013-10-22 15:19:52 +0200

    Add a cxx_variadic_templates feature.

--- Modules/Compiler/MSVC-CXX-FeatureTests.cmake
+++ Modules/Compiler/MSVC-CXX-FeatureTests.cmake
@@ -1,4 +1,5 @@
 
 set(_cmake_feature_test_cxx_delegating_constructors "_MSC_VER >= 1800")
+set(_cmake_feature_test_cxx_variadic_templates "_MSC_VER >= 1800")
 
 set(_cmake_feature_test_msvcxx_sealed "_MSC_VER >= 1400")
