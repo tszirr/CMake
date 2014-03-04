4d02ac9ae83176c01a495f8bcb45a15af5acf4d5
Brad King <brad.king@kitware.com>
2014-03-04 09:47:28 -0500

    Revert topic 'dev/faster-evis'
    It will be revised and restored again.

--- Tests/RunCMake/CMP0052/RunCMakeTest.cmake
+++
@@ -1,5 +0,0 @@
-include(RunCMake)
-
-run_cmake(CMP0052-At-OLD)
-run_cmake(CMP0052-At-NEW)
-run_cmake(CMP0052-At-WARN)
--- Tests/RunCMake/Syntax/ParenInVarName1.cmake
+++
@@ -1,4 +0,0 @@
-cmake_policy(SET CMP0052 OLD)
-
-set("{e(x)}" value)
-message("-->${e(x)}<--")
