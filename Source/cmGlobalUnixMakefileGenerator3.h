46019f89e555341c2b42d824e947c8a9e1d32751
Stephen Kelly <steveire@gmail.com>
2014-03-11 20:39:01 +0100

    cmGeneratorTarget: Compute target objects in LookupObjectLibraries.
    Remove the old method from the generators which are now all
    functionally identical.
    
    Remove the supporting cmGeneratorTarget::AddObject method.
    
    In a follow-up, the LookupObjectLibraries may be replaced by a
    true generator expression evaluator for TARGET_OBJECTS. That
    will require generators to use cmGeneratorTarget::GetExternalObjects
    which is not currently the case for Xcode and VS generators.

--- Source/cmGlobalUnixMakefileGenerator3.h
+++ Source/cmGlobalUnixMakefileGenerator3.h
@@ -198,7 +198,6 @@ protected:
 private:
   virtual const char* GetBuildIgnoreErrorsFlag() const { return "-i"; }
   virtual std::string GetEditCacheCommand() const;
-  virtual void ComputeTargetObjects(cmGeneratorTarget* gt) const;
 };
 
 #endif
