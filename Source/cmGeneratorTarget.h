32a6fdb59e1beb36c567a7fcfa83c7eebdef03e7
Stephen Kelly <steveire@gmail.com>
2014-03-12 00:11:18 +0100

    cmGeneratorTarget: Remove GetObjectName method.
    It is no longer used.

--- Source/cmGeneratorTarget.h
+++ Source/cmGeneratorTarget.h
@@ -33,7 +33,6 @@ public:
   void GetSourceFiles(std::vector<cmSourceFile*>& files) const;
 
   void GetObjectSources(std::vector<cmSourceFile*> &) const;
-  const std::string& GetObjectName(cmSourceFile const* file);
 
   void AddObject(cmSourceFile *sf, std::string const&name);
 
