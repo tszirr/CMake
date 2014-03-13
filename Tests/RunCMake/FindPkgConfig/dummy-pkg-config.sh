6c5d5c16633c9784387b04186c4cc852d1539a1e
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2014-03-13 18:27:28 +0100

    FindPkgConfig: Add unit tests

---
+++ Tests/RunCMake/FindPkgConfig/dummy-pkg-config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This is a replacement for pkg-config that compares the string passed
+# to the --exists argument with the PKG_CONFIG_PATH environment variable
+# and returns 1 if they are different.
+
+case $1 in
+  --version)
+    echo "0.0-cmake-dummy"
+    ;;
+  --exists)
+    [ "$2" = "${PKG_CONFIG_PATH}" ] || exit 1
+    ;;
+  *)
+    exit 255
+    ;;
+esac
