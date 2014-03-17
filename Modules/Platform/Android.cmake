986c6f69576e796a1716ebd0f7908c1394c882a8
Stephen Kelly <steveire@gmail.com>
2014-03-17 15:11:14 +0100

    Remove setting of Android variable.

--- Modules/Platform/Android.cmake
+++ Modules/Platform/Android.cmake
@@ -3,4 +3,3 @@ set(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "")
 # RPath is useless on Android, because we can't determine the installation
 # location ahead of time.
 set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")
-set(Android 1)
