f496d842399f4087927e81b1426370873bb77548
Stephen Kelly <steveire@gmail.com>
2014-03-19 13:32:59 +0100

    Add cxx_override feature.

--- Modules/Compiler/Clang-CXX-FeatureTests.cmake
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -13,4 +13,5 @@ unset(testable_features)
 
 set(_cmake_feature_test_gnuxx_typeof "!defined(__STRICT_ANSI__)")
 set(_cmake_feature_test_cxx_final "__has_extension(cxx_override_control)")
+set(_cmake_feature_test_cxx_override "__has_extension(cxx_override_control)")
 set(_cmake_feature_test_cxx_binary_literals "1")
--- Modules/Compiler/MSVC-CXX-FeatureTests.cmake
+++ Modules/Compiler/MSVC-CXX-FeatureTests.cmake
@@ -7,3 +7,4 @@ set(_cmake_feature_test_msvcxx_sealed "_MSC_VER >= 1400")
 set(_cmake_feature_test_cxx_static_assert "_MSC_VER >= 1600")
 
 set(_cmake_feature_test_cxx_final "_MSC_VER >= 1700")
+set(_cmake_feature_test_cxx_override "_MSC_VER >= 1400")
