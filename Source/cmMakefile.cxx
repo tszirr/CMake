ef0360a9a0dbcc11aa7f78178fd804986f20f9f8
Stephen Kelly <steveire@gmail.com>
2014-04-02 15:02:41 +0200

    Features: Add cxx_variadic_templates.

--- Source/cmMakefile.cxx
+++ Source/cmMakefile.cxx
@@ -42,7 +42,8 @@
 #include <assert.h>
 
 #define FOR_EACH_CXX_FEATURE(F) \
-  F(cxx_delegating_constructors)
+  F(cxx_delegating_constructors) \
+  F(cxx_variadic_templates)
 
 class cmMakefile::Internals
 {
