d58ca38f4cd928c9d81b3c3878905f6b0fc94315
Stephen Kelly <steveire@gmail.com>
2014-04-06 10:07:26 +0200

    Features: Expect cxx_variadic_templates to implement N2555.
    N2555 is essentially a bugfix and predates most compiler releases which
    aimed to experimentally support variadic templates.

--- Modules/Compiler/GNU-CXX-FeatureTests.cmake
+++ Modules/Compiler/GNU-CXX-FeatureTests.cmake
@@ -17,6 +17,11 @@ set(_cmake_feature_test_cxx_nonstatic_member_init "${GNU47_CXX11}")
 set(_cmake_feature_test_cxx_noexcept "${GNU47_CXX11}")
 set(_cmake_feature_test_cxx_alias_templates "${GNU47_CXX11}")
 set(_cmake_feature_test_cxx_user_literals "${GNU47_CXX11}")
+# NOTE: C++11 was ratified in September 2011. GNU 4.7 is the first minor
+# release following that (March 2012), and the first minor release to
+# support -std=c++11. Prior to that, support for C++11 features is technically
+# experiemental and possibly incomplete (see for example the note below about
+# cxx_variadic_template_template_parameters)
 # TODO: Should be supported by GNU 4.6
 set(GNU46_CXX11 "${_oldestSupported} && __cplusplus >= 201103L")
 set(_cmake_feature_test_cxx_constexpr "${GNU46_CXX11}")
@@ -41,6 +46,13 @@ set(_cmake_feature_test_cxx_extern_templates "${GNU44_CXX11}")
 set(_cmake_feature_test_cxx_generalized_initializers "${GNU44_CXX11}")
 set(_cmake_feature_test_cxx_uniform_initialization "${GNU44_CXX11}")
 set(_cmake_feature_test_cxx_inline_namespaces "${GNU44_CXX11}")
+# TODO: If features are ever recorded for GNU 4.3, there should possibly
+# be a new feature added like cxx_variadic_template_template_parameters,
+# which is implemented by GNU 4.4, but not 4.3. cxx_variadic_templates is
+# actually implemented by GNU 4.3, but variadic template template parameters
+# 'completes' it, so that is the version we record as having the variadic
+# templates capability in CMake. See
+# http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2008/n2555.pdf
 # TODO: Should be supported by GNU 4.3
 set(GNU43_CXX11 "${_oldestSupported} && __cplusplus >= 201103L")
 set(_cmake_feature_test_cxx_static_assert "${GNU43_CXX11}")
